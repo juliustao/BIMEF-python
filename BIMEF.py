e0de94852c07430371dd95ab28c39699cdc878f5
Julius Tao <juliusltao@gmail.com>
2019-04-15 15:40:49 -0400

    debugged up until cholesky decomposition

--- BIMEF.py
+++ BIMEF.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from imresize import imresize
 from scipy import signal
 from scipy.sparse import spdiags
 from scipy.sparse.linalg import cg
@@ -18,10 +19,10 @@ def BIMEF(I, mu=0.5, k=None, a=-0.3293, b=1.1258):
     """
 
     def maxEntropyEnhance(I, isBad=None):
-        Y = rgb2gm(np.real(np.maximum(cv2.resize(I, dsize=(50, 50), interpolation=cv2.INTER_CUBIC), 0)))
+        Y = rgb2gm(np.real(np.maximum(cv2.resize(I, dsize=(50, 50), fx=0, fy=0, interpolation=cv2.INTER_CUBIC), 0)))
 
         if not (isBad is None):
-            isBad = cv2.resize(isBad, dsize=(50, 50), interpolation=cv2.INTER_CUBIC).T
+            isBad = cv2.resize(isBad, dsize=(50, 50), fx=0, fy=0, interpolation=cv2.INTER_CUBIC).T
             Y = Y[isBad]
             Y = np.reshape(Y, (Y.size, 1))
 
@@ -35,6 +36,7 @@ def BIMEF(I, mu=0.5, k=None, a=-0.3293, b=1.1258):
 
         return J
 
+    I0 = I
     I = im2double(I)
 
     lamb = 0.5
@@ -42,8 +44,7 @@ def BIMEF(I, mu=0.5, k=None, a=-0.3293, b=1.1258):
 
     # t: scene illumination map
     t_b = np.amax(I, axis=2)
-    t_our = cv2.resize(tsmooth(cv2.resize(t_b, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC), lamb, sigma),
-                       dsize=np.shape(t_b), interpolation=cv2.INTER_CUBIC)
+    t_our = imresize(tsmooth(imresize(t_b, scalar_scale=0.5), lamb, sigma), t_b.shape)
     # We try to replicate MatLab's imresize function, which uses intercubic interpolation and anti-aliasing by default
 
     # k: exposure ratio
@@ -88,15 +89,15 @@ def tsmooth(I, lamb=0.01, sigma=3.0, sharpness=0.001):
 
 
 def computeTextureWeights(fin, sigma, sharpness):
-    h1 = np.matrix(np.diff(fin, axis=1)).H
-    h2 = np.matrix(fin[:, 0]).H - np.matrix(fin[:, -1]).H
-    dt0_h = np.matrix(np.concatenate((h1, h2), axis=0)).H
     v1 = np.diff(fin, axis=0)
     v2 = np.expand_dims(fin[0, :] - fin[-1, :], axis=0)
-    dt0_v = np.matrix(np.concatenate((v1, v2), axis=0))
+    dt0_v = np.concatenate((v1, v2), axis=0)
+    h1 = np.matrix(np.diff(fin, axis=1)).H
+    h2 = np.matrix(np.expand_dims(fin[:, 0], axis=1)).H - np.matrix(np.expand_dims(fin[:, -1], axis=1)).H
+    dt0_h = np.matrix(np.concatenate((h1, h2), axis=0)).H
 
-    gauker_h = signal.convolve2d(dt0_h, np.rot90(np.ones((1, sigma))), mode='valid')
-    gauker_v = signal.convolve2d(dt0_v, np.rot90(np.ones((sigma, 1))), mode='valid')
+    gauker_h = signal.convolve2d(dt0_h, np.ones((1, sigma)), mode='same')
+    gauker_v = signal.convolve2d(dt0_v, np.ones((sigma, 1)), mode='same')
     W_h = np.multiply(np.absolute(gauker_h), np.absolute(dt0_h)) + sharpness
     W_h = np.divide(1, W_h)
     W_v = np.multiply(np.absolute(gauker_v), np.absolute(dt0_v)) + sharpness
@@ -106,32 +107,35 @@ def computeTextureWeights(fin, sigma, sharpness):
 
 
 def solveLinearEquation(IN, wx, wy, lamb):
+    if len(IN.shape) == 2:
+        IN = np.expand_dims(IN, axis=2)
     r, c, ch = IN.shape
     k = r * c
-    dx = -lamb * np.reshape(wx, (1, wx.size))  # row vector; in MatLab this is a column vector
-    dy = -lamb * np.reshape(wy, (1, wy.size))  # row vector
-    tempx = np.concatenate((wx[:, -1], wx[:, 0:-2]), axis=1)
-    tempy = np.concatenate((wy[-1, :], wy[0:-2, :]), axis=0)
-    dxa = -lamb * np.reshape(tempx, (1, tempx.size))  # row vector
-    dya = -lamb * np.reshape(tempy, (1, tempy.size))  # row vector
+    dx = -lamb * np.reshape(wx, (wx.size, 1), order='F')
+    dy = -lamb * np.reshape(wy, (wy.size, 1), order='F')
+    tempx = np.concatenate((wx[:, -1], wx[:, 0:-1]), axis=1)
+    tempy = np.concatenate((np.expand_dims(wy[-1, :], axis=0), wy[0:-1, :]), axis=0)
+    dxa = -lamb * np.reshape(tempx, (tempx.size, 1), order='F')
+    dya = -lamb * np.reshape(tempy, (tempy.size, 1), order='F')
     tempx = np.concatenate((wx[:, -1], np.zeros((r, c-1))), axis=1)
-    tempy = np.concatenate((wy[-1, :], np.zeros((r-1, c))), axis=0)
-    dxd1 = -lamb * np.reshape(tempx, (1, tempx.size))  # row vector
-    dyd1 = -lamb * np.reshape(tempy, (1, tempy.size))  # row vector
+    tempy = np.concatenate((np.expand_dims(wy[-1, :], axis=0), np.zeros((r-1, c))), axis=0)
+    dxd1 = -lamb * np.reshape(tempx, (tempx.size, 1), order='F')
+    dyd1 = -lamb * np.reshape(tempy, (tempy.size, 1), order='F')
     wx[:, -1] = 0
     wy[-1, :] = 0
-    dxd2 = -lamb * np.reshape(wx, (1, wx.size))  # row vector
-    dyd2 = -lamb * np.reshape(wy, (1, wy.size))  # row vector
+    dxd2 = -lamb * np.reshape(wx, (wx.size, 1), order='F')
+    dyd2 = -lamb * np.reshape(wy, (wy.size, 1), order='F')
 
-    Ax = spdiags(np.concatenate((dxd1, dxd2), axis=0), [-k+r, -r], k, k)
-    Ay = spdiags(np.concatenate((dyd1, dyd2), axis=0), [-r+1, -1], k, k)
+    Ax = spdiags(np.concatenate((dxd1, dxd2), axis=1).T, [-k+r, -r], k, k)
+    Ay = spdiags(np.concatenate((dyd1, dyd2), axis=1).T, [-r+1, -1], k, k)
     # diagonals stored row-wise; in MatLab the diagonals are stored column-wise
 
-    D = 1 - (dx + dy + dxa + dya)
+    D = 1 - (dx + dy + dxa + dya)  # column vector
 
-    A = (Ax + Ay) + np.matrix(Ax + Ay).H + spdiags(D, 0, k, k)
+    Axy = Ax + Ay
+    A = Axy + Axy.T + spdiags(D.T, 0, k, k)
 
-    fast = False
+    fast = True
     if fast:  # This method uses approximations to solve A*x=tin(:) more quickly
         L = np.linalg.cholesky(A)  # what happens if we instead use scipy's cholesky function?
         OUT = IN
@@ -154,9 +158,8 @@ def solveLinearEquation(IN, wx, wy, lamb):
 
 
 def im2double(im):
-    dtype = im.dtype
-    if dtype is np.float64:
+    if im.dtype == np.float64:
         return im  # do nothing if the image array has already been converted to floating points
     else:
-        info = np.iinfo(dtype)  # Get the data type of the input image
+        info = np.iinfo(im.dtype)  # Get the data type of the input image
         return im.astype(np.float64) / info.max  # Divide all values by the largest possible value in the datatype
--- experiments.py
+++ experiments.py
@@ -1,10 +1,19 @@
 import cv2
 import BIMEF
+from scipy.io import loadmat
+"""
+img_path = '/Users/carnitas/BIMEF/data/TEST/0cd9b7d9-36b5-4b73-a29d-5ed319eb007d.jpg'
 
-img_path = '/Users/carnitas/Downloads/dark_images/0cd9b7d9-36b5-4b73-a29d-5ed319eb007d.jpg'
 bgr_img = cv2.imread(img_path, 1)
 b, g, r = cv2.split(bgr_img)       # get b,g,r
 rgb_img = cv2.merge([r, g, b])     # switch it to rgb
+"""
+
+data_path = '/Users/carnitas/BIMEF/image.mat'
+
+data = loadmat(data_path)
+
+rgb_img = data['image']
 
 enhanced_img = BIMEF.BIMEF(rgb_img)
 cv2.imwrite('/Users/carnitas/Matlab2Python/0cd9b7d9-36b5-4b73-a29d-5ed319eb007d_python_BIMEF.jpg', enhanced_img)
